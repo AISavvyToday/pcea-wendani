e164c786a4d1b20b1507367e7bc60723af712d59
yoyoyousef1 <yhassan@worcester.edu>
2025-12-22 16:39:30 +0300

    debugging enhancements

---
+++ reports/templatetags/custom_filters.py
@@ -0,0 +1,18 @@
+# reports/templatetags/custom_filters.py
+from django import template
+
+register = template.Library()
+
+@register.filter
+def divide(value, arg):
+    try:
+        return float(value) / float(arg)
+    except (ValueError, ZeroDivisionError):
+        return 0
+
+@register.filter
+def multiply(value, arg):
+    try:
+        return float(value) * float(arg)
+    except (ValueError, TypeError):
+        return 0
