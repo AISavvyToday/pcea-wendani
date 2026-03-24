c97382f76800f22e46a53b10377b1d77fb0f98e6
AISavvyToday <jacmuhuri@gmail.com>
2026-03-24 08:03:11 +0300

    comms admin

--- communications/admin.py
+++ communications/admin.py
@@ -1,11 +1,10 @@
 # communications/admin.py
 
 from django.contrib import admin
+from .models import Announcement, EmailNotification, NotificationTemplate, SMSNotification
 
-# Communications models will be added later
-# This is a placeholder for SMS/Email templates and logs
 
-# Example structure for future:
-# @admin.register(SMSTemplate)
-# @admin.register(EmailTemplate)
-# @admin.register(NotificationLog)
+@admin.register(Announcement)
+@admin.register(EmailNotification)
+@admin.register(NotificationTemplate)
+@admin.register(SMSNotification)
